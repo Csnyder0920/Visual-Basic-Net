feda0eaf2abd003f933ce561cdbba0be3c373c6d
Csnyder0920 <33033510+Csnyder0920@users.noreply.github.com>
2017-10-31 11:29:33 -0500

    Added HotelProgram.sln

---
+++ Chapter 1/HotelProgram/HotelProgram/HotelProgram/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Header.Click
+
+    End Sub
+
+    Private Sub Directions_Click(sender As Object, e As EventArgs) Handles Directions.Click
+        lblDirections.Visible = False
+        DisplayDirections.Visible = True
+        Directions.Visible = False
+    End Sub
+
+    Private Sub DisplayDirections_Click(sender As Object, e As EventArgs) Handles DisplayDirections.Click
+        lblDirections.Visible = True
+        DisplayDirections.Visible = False
+        Directions.Visible = True
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
