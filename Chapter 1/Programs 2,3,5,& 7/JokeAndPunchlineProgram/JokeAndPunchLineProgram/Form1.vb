b49c55f8b688ebb2c8d9d1c28ed43263c3f76d3c
Christian Snyder <Csnyder0920@gmail.com>
2017-11-03 16:36:43 -0500

    Added JokeAndPunchLineProgram

--- Chapter 1/Programs 2,3,5,& 7/JokeAndPunchlineProgram/JokeAndPunchLineProgram/Form1.vb
+++ Chapter 1/Programs 2,3,5,& 7/JokeAndPunchlineProgram/JokeAndPunchLineProgram/Form1.vb
@@ -1,1 +1,12 @@
-﻿
+﻿Private Sub btnSetup_Click(sender As Object, e As EventArgs) Handles btnSetup.Click
+    lblSetup.Visible = True
+End Sub
+
+Private Sub btnPunchLine_Click(sender As Object, e As EventArgs) Handles btnPunchLine.Click
+    lblPunchLine.Visible = True
+End Sub
+
+Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+    Me.Close()
+End Sub
+End Class
