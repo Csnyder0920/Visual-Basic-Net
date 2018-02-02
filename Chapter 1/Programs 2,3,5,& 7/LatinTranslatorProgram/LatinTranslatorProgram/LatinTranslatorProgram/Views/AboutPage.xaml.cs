6bff55e01907b50b35861ece26a82a6fd40e89e2
Christian Snyder <Csnyder0920@gmail.com>
2018-02-01 20:22:29 -0600

    Update

---
+++ Chapter 1/Programs 2,3,5,& 7/LatinTranslatorProgram/LatinTranslatorProgram/LatinTranslatorProgram/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace LatinTranslatorProgram
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
